6679e32b0a11cff75df0c6c5a7467bddf25716be
nnqq <43930873+nnqq@users.noreply.github.com>
2020-10-29 10:47:14 +0300

    wip

--- redis/client.go
+++ redis/client.go
@@ -4,6 +4,7 @@ import (
 	rd "github.com/go-redis/redis/v7"
 	"github.com/nnqq/scr-api/config"
 	"github.com/nnqq/scr-api/logger"
+	"time"
 )
 
 var Client *rd.ClusterClient
@@ -17,4 +18,20 @@ func init() {
 	logger.Must(err)
 
 	Client = rdb
+
+	go func() {
+		for {
+			time.Sleep(time.Second)
+
+			e := Client.Ping().Err()
+			if e != nil {
+				logger.Log.Error().Err(e).Send()
+				Client = rd.NewClusterClient(&rd.ClusterOptions{
+					Addrs: []string{config.Env.Redis.ClusterURL},
+				})
+
+				logger.Must(Client.Ping().Err())
+			}
+		}
+	}()
 }
